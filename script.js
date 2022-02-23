33854d8738405f7f1404078d6a76f2bcc7af1c16
Cigmaian Andrei <cigmaian1@gmail.com>
2022-02-23 21:05:22 +0200

    script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const closePopUp = document.getElementsByTagName('span')[0];
 closePopUp.addEventListener('click', () => popUpForm.style.display = 'none');
 
 //Book Constructor
-class Book {
+let Book = class {
     constructor(title, author, pages, read) {
         this.title = form.title.value; 
         this.author = form.author.value; 
@@ -20,7 +20,6 @@ class Book {
     }
 }
 
-//creates book from Book Constructor, adds to library
 let myLibrary = [];
 let newBook;
 
@@ -28,9 +27,9 @@ function addBookToLibrary() {
     event.preventDefault();
     popUpForm.style.display = 'none';
 
-    newBook = new Book(title, author, pages,read); 
+    newBook = new Book(title, author, pages, read); 
     myLibrary.push(newBook); 
-    setData();  //saves updated array in local storage
+    setData();  
     render(); 
     form.reset();
 }
@@ -62,18 +61,18 @@ function createBook(item) {
 
     titleDiv.textContent = item.title;
     titleDiv.classList.add('title');
-    bookDiv.appendChild(titleDiv);
+    bookDiv.append(titleDiv);
 
     authDiv.textContent = item.author;
     authDiv.classList.add('author');
-    bookDiv.appendChild(authDiv);
+    bookDiv.append(authDiv);
 
     pageDiv.textContent = item.pages;
     pageDiv.classList.add('pages');
-    bookDiv.appendChild(pageDiv);
+    bookDiv.append(pageDiv);
 
     readBtn.classList.add('readBtn')    
-    bookDiv.appendChild(readBtn);
+    bookDiv.append(readBtn);
     if(item.read===false) {
         readBtn.textContent = 'Not Read';
         readBtn.style.backgroundColor = '#e04f63';
@@ -89,7 +88,7 @@ function createBook(item) {
 
     removeBtn.textContent = 'Remove'; 
     removeBtn.setAttribute('id', 'removeBtn');
-    bookDiv.appendChild(removeBtn);
+    bookDiv.append(removeBtn);
 
     removeBtn.addEventListener('click', () => {
         myLibrary.splice(myLibrary.indexOf(item),1);
@@ -97,9 +96,40 @@ function createBook(item) {
         render();
     });
 
-
-    library.appendChild(bookDiv);
+    library.append(bookDiv);
 };
+
+// search 
+
+const userCardTemplate = document.querySelector("[data-user-template]")
+const userCardContainer = document.querySelector("[data-user-cards-container]")
+const searchInput = document.querySelector("[data-search]")
+
+let users = []
+
+searchInput.addEventListener("input", e => {
+  const value = e.target.value.toLowerCase()
+  users.forEach(user => {
+    const isVisible =
+      user.name.toLowerCase().includes(value) ||
+      user.address.toLowerCase().includes(value)
+    user.element.classList.toggle("hide", !isVisible)
+  })
+})
+
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then(res => res.json())
+  .then(data => {
+    users = data.map(user => {
+      const card = userCardTemplate.content.cloneNode(true).children[0]
+      const header = card.querySelector("[data-header]")
+      const body = card.querySelector("[data-body]")
+      header.textContent = user.name
+      body.textContent = user.address.city
+      userCardContainer.append(card)
+      return { name: user.name, address: user.address.city, element: card }
+    })
+  })
 
 // setting Library to be stored in local storage
 function setData() {
@@ -111,7 +141,7 @@ function restore() {
     if(!localStorage.myLibrary) {
         render();
     }else {
-        let objects = localStorage.getItem('myLibrary') // gets information from local storage to use in below loop to create DOM/display
+        let objects = localStorage.getItem('myLibrary') 
         objects = JSON.parse(objects);
         myLibrary = objects;
         render();
@@ -119,3 +149,6 @@ function restore() {
 }
 
 restore();
+
+
+  
